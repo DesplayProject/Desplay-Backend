ccc570b5200aa428c74c8b4904e6e42d5eaeb3af
moodTRBL <sonickim6679@gmail.com>
2024-09-30 23:04:45 +0900

    .

--- src/test/kotlin/com/deterior/DeteriorApplicationTests.kt
+++ src/test/kotlin/com/deterior/DeteriorApplicationTests.kt
@@ -2,7 +2,9 @@ package com.deterior
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 
+@TestPropertySource(locations = ["classpath:application.yml"])
 @SpringBootTest
 class DeteriorApplicationTests {
 
