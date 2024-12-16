32875965688d37e76ca11eb26e8f941678f169f5
moodTRBL <sonickim6679@gmail.com>
2024-12-16 15:34:33 +0900

    CORS 설정 클래스 추가(임시)

---
+++ src/main/kotlin/com/deterior/global/config/WebConfig.kt
@@ -0,0 +1,18 @@
+package com.deterior.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    @Profile("deploy")
+    override fun addCorsMappings(registry: CorsRegistry) {
+        //도메인 네임을 가지면 그때 수정
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowCredentials(true)
+    }
+}
