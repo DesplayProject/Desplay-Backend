1e3f13aee5108ad982725be4b88df075337ad928
moodTRBL <sonickim6679@gmail.com>
2025-01-24 01:51:38 +0900

    로깅 필터에서 Multipart data를 제대로 전달하지 못하는 문제 해결

--- src/main/kotlin/com/deterior/global/filter/LoggingFilter.kt
+++ src/main/kotlin/com/deterior/global/filter/LoggingFilter.kt
@@ -1,22 +1,12 @@
 package com.deterior.global.filter
 
-import com.deterior.global.log.CachedBodyHttpServletRequest
-import com.deterior.global.util.LogUtils
-import com.deterior.logger
+import com.deterior.global.log.StreamCachingRequestWrapper
 import jakarta.servlet.FilterChain
-import jakarta.servlet.ServletRequest
-import jakarta.servlet.ServletResponse
-import jakarta.servlet.annotation.WebFilter
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.slf4j.MDC
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.core.Ordered
-import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
-import org.springframework.web.filter.GenericFilterBean
 import org.springframework.web.filter.OncePerRequestFilter
-import org.springframework.web.util.ContentCachingRequestWrapper
 import org.springframework.web.util.ContentCachingResponseWrapper
 import java.util.*
 
@@ -29,7 +19,7 @@ class LoggingFilter : OncePerRequestFilter() {
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-        val requestWrapper = ContentCachingRequestWrapper(request)
+        val requestWrapper = StreamCachingRequestWrapper(request)
         val responseWrapper = ContentCachingResponseWrapper(response)
         val uuid = UUID.randomUUID().toString()
         MDC.put("requestId", uuid)
