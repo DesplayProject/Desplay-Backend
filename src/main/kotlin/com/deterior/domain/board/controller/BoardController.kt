e4097afd2c810e3adac8880b386430de8081505c
moodTRBL <sonickim6679@gmail.com>
2025-03-13 19:51:01 +0900

    자동완성 로직 수정

--- src/main/kotlin/com/deterior/domain/board/controller/BoardController.kt
+++ src/main/kotlin/com/deterior/domain/board/controller/BoardController.kt
@@ -17,7 +17,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
-import java.io.File
 
 @Controller
 @RequestMapping("/api")
